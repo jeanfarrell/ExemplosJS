38cb7c07098bf50dc2a85fc0efdb71612190daea
Jean Farrell <jeanfarrell@gmail.com>
2018-11-21 07:29:52 -0200

    sunbido

--- index.php
+++ index.php
@@ -10,8 +10,24 @@ echo "<pre>";
 
 
 
-print_r(json_decode($eve, true));
+#print_r(json_decode($eve, true));
 print_r(json_decode($mod, true));
-print_r(json_decode($cat, true));
+#print_r(json_decode($cat, true));
+
+
+$array1=array(
+				"categorias" => array("11111" => array("id_categoria"=>11111, "ds_categoria"=>"categoria1", "tags"=>array("esports"=>"esports","games"=>"games","cultura"=>"cultura")), 
+			 						"22222" => array("id_categoria"=>22222, "ds_categoria"=>"categoria2", "tags"=>array("esports","games","cultura"))
+			 						)
+			);
+
+print_r(json_encode($array1));
+
+$obj=json_encode($array1);
+
+echo "<br>";
+
+
+print_r(json_decode($obj, true));
 
 ?>
