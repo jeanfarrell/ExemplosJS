ac3998fb8ee602c690fd74dff777f6db5d62fc59
Jean Farrell <jeanfarrell@gmail.com>
2019-03-22 01:03:05 -0300

    fghfgh

--- index.php
+++ index.php
@@ -1,33 +1,20 @@
 <?php
 
 
-$eve = file_get_contents("eventos.txt");
-$mod = file_get_contents("modalidades.txt");
-$cat = file_get_contents("categorias.txt");
-
+$data = file_get_contents("dados.txt");
+$objeto = json_decode($data, true);
 
 echo "<pre>";
 
 
+print_r(json_decode($data, true));
 
-#print_r(json_decode($eve, true));
-print_r(json_decode($mod, true));
-#print_r(json_decode($cat, true));
+$array = json_encode($objeto, JSON_PRETTY_PRINT);
 
-
-$array1=array(
-				"categorias" => array("11111" => array("id_categoria"=>11111, "ds_categoria"=>"categoria1", "tags"=>array("esports"=>"esports","games"=>"games","cultura"=>"cultura")), 
-			 						"22222" => array("id_categoria"=>22222, "ds_categoria"=>"categoria2", "tags"=>array("esports","games","cultura"))
-			 						)
-			);
-
-print_r(json_encode($array1));
-
-$obj=json_encode($array1);
-
-echo "<br>";
+print_r($array);
 
 
-print_r(json_decode($obj, true));
 
-?>
+
+
+
